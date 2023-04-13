53886a6c1b338eb0de6042da55f3df7f95c33237
miltonralves <miltonralves@gmail.com>
2023-04-13 19:18:31 -0300

    first commit

---
+++ src/PageTitle/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function PageTitle({ children }) {
+   return (
+      <>
+         <Head>
+            <title>{children}</title>
+         </Head>
+      </>
+   );
+}
